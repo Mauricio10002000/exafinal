990dfe7956e437e8d00d59ff46e6fbbe0d8180c9
Mauricio <maurodre44@gmail.com>
2023-12-12 22:04:34 -0400

    first commit

--- src/modelo/Coneccion.py
+++ src/modelo/Coneccion.py
@@ -1,13 +1,12 @@
-import pymysql
-
+import psycopg2
+## para recuperar datos de .env
 from decouple import config
 
 def conexion2023():
-    conex=pymysql.connect(
-                        host=config('BD_HOST'),
-                        user=config('BD_USER'),
-                        password=config('BD_PASSWORD'),
-                        database=config('BD_DB'),
-                        port=3309
-                        )
+    conex=psycopg2.connect(
+            host=config('PG_HOST'),
+            user=config('PG_USER'),
+            password=config('PG_PASSWORD'),
+            database=config('PG_DB')
+        )
     return conex
